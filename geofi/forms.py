aff897441cba20295bff29046b2fd2c95796920c
dlDreyfus <dreyfus.dl@gmail.com>
2026-04-30 09:34:00 -0300

    Inclusão de Dropdowns com filtros em cascata para as telas de edição e inclusão de novo registro

--- geofi/forms.py
+++ geofi/forms.py
@@ -1,6 +1,38 @@
 from django import forms
 from .models import RegistroFinanceiro
 from django.core.exceptions import ValidationError
+import sqlite3
+import os
+from django.conf import settings
+
+FIELD_MAPPING = {
+    'rf_sub': 'RF_SUB',
+    'unidade_coordenacao': 'UNID_COORD',
+    'grupos': 'GRUPO',
+    'despesa_gerencial': 'DESP_GERENCIAL',
+    'iniciativa': 'INICIATIVA',
+    'gnd': 'GND',
+    'tipo_despesa': 'TIPO_DESPESA',
+    'po': 'PO',
+    'acao': 'ACAO',
+    'po_gnd': 'PO_GND',
+}
+
+def get_opcoes(campo_db):
+    db_path = os.path.join(settings.BASE_DIR, 'db.filtrosRO')
+    opcoes = [('', 'Selecione')]
+    if not os.path.exists(db_path):
+        return opcoes
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT valor FROM Filtros WHERE campo = ? ORDER BY valor", (campo_db,))
+        for row in cursor.fetchall():
+            opcoes.append((row[0], row[0]))
+        conn.close()
+    except Exception as e:
+        pass
+    return opcoes
 
 class RegistroFinanceiroForm(forms.ModelForm):
     class Meta:
@@ -14,3 +46,8 @@ class RegistroFinanceiroForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['placeholder'] = field.label or field_name
+            if field_name in FIELD_MAPPING:
+                opcoes = get_opcoes(FIELD_MAPPING[field_name])
+                self.fields[field_name].widget = forms.Select(choices=opcoes)
+                self.fields[field_name].widget.attrs['class'] = 'select2-field form-control'
+                self.fields[field_name].widget.attrs['style'] = 'padding: 10px; border: 1px solid #ccc; border-radius: 4px; font-size: 1rem; width: 100%; background-color: #fff;'
